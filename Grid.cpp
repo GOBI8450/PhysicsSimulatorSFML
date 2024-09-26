176fe03ae216e3baabd3da74362ad3f921448908
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2024-09-26 07:47:14 +0300

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -1,142 +1,111 @@
 #pragma once
-#include <random> 
-#include "Grid.h"
-#include "CircleBase.h";
+#include <unordered_map>;
 #include <iostream> using namespace std;
-#include <thread>
-#include <functional>
+#include "CircleBase.h";
+#include <random>;  // For random number generation
+#include <ctime>;   // For seeding with current time
 
-
-class ObjectsList
+class Grid //Using Hash Maping
 {
 private:
-    int ballCount = 0;
-    std::mt19937 rnd;
-    Grid grid;
+	std::unordered_map<int, std::vector<CircleBase*>> gridMap;// int for Key, and std::vector<CircleBase*>> for type of object
+	std::mt19937 rnd;//random variable
+	int ballCount = 0;
+	std::vector<int> hashKeyVec;
+
+	//Hash Function for unordered_map
+	int hashFunction(int x, int y) const {
+		return x + y * 1000000007; //hashing this, prime number to make y stand out from x so no hash collision
+	}
+
 
 public:
-    std::vector<CircleBase*> ballsList;
+	Grid() {}; //Constructor
 
-    ObjectsList() { // Adjust cell size as needed
-        rnd.seed(static_cast<unsigned>(std::time(nullptr)));
-    }
-    ~ObjectsList() {
-        for (auto ball : ballsList) {
-            delete ball;
-        }
-    }
-    
-    void DeleteAll() {
-        for (size_t i = 0; i < ballsList.size(); i++)
-        {
-            ballsList.clear();
-        }
-    }
+	//Insert the circle by getting the hash Key and putting it in the unordered map 
+	void InsertCircle(CircleBase* circle) {
+		sf::Vector2f pos = circle->getPosition();
+		float multiplier = 2.5;// !still need to work on the multipier for best performance!
+		int gridX = static_cast<int>(pos.x / (circle->getRadius() * multiplier)); 
+		int gridY = static_cast<int>(pos.y / (circle->getRadius() * multiplier));
+		int hashKey = hashFunction(gridX, gridY);
+		hashKeyVec.push_back(hashKey);
+		gridMap[hashKey].push_back(circle);
+	}
+	
+	//Clears the gridMap 
+	void clear() {
+		gridMap.clear();
+	}
 
-    void CreateNewCircle(float gravity, sf::Color color,sf::Vector2f pos) {
-        std::uniform_int_distribution<int> radiusRange(2, 2); 
-        std::uniform_int_distribution<int> massRange(1, 1);
-        std::uniform_int_distribution<int> rndXRange(300, 500);  // Replace 920 with actual window width
-        //std::uniform_int_distribution<int> rndYRange(50, 1280 - 50); // Replace 1280 with actual window height
-        sf::Vector2f position(pos);
+	//returns the hash map size
+	int GetHashMapSize() {
+		return gridMap.size();
+	}
 
-        CircleBase* ball = new CircleBase(radiusRange(rnd), color, position, gravity, massRange(rnd));
-        ballsList.push_back(ball);
-        ballCount += 1;
-        //std::cout << "Creating ball at position: (" << position.x << ", " << position.y << ")\n";
-    }
+	//because hashing is non reversable we give all the keys for external use to easy accsess to every gridMap vectors
+	std::vector<int> GetAllHashKeys() {
+		return hashKeyVec;
+	}
 
-    void HandleCollisionsInRange(sf::RenderWindow& window, float elastic, std::vector<CircleBase*> vecObj) {
-        if (elastic==0)
-        {
-            for (auto& ball : vecObj) {
-                for (size_t i = 0; i < 2; i++)
-                {
-                    ball->handleWallCollision(window);
-                }
-                std::vector<CircleBase*> potentialCollisions = grid.GetNerbyCellsObjects(ball);
-                for (auto& otherBall : potentialCollisions) {
-                    if (&ball != &otherBall) {
-                        for (size_t i = 0; i < 1; i++)
-                        {
-                            ball->handleCollision(otherBall);
-                        }
-                    }
-                }
-            }
-        }
-        else {
-            for (auto& ball : ballsList) {
-                ball->handleWallCollision(window);
-                std::vector<CircleBase*> potentialCollisions = grid.GetNerbyCellsObjects(ball);
-                for (auto& otherBall : potentialCollisions) {
-                    if (&ball != &otherBall) {
-                        ball->handleCollisionElastic(otherBall,elastic);
-                    }
-                }
-            }
-        }
+	//turn a hash keys vector to a vector that contains vectors that contains CircleBase
+	std::vector<std::vector<CircleBase*>> GetCircelsVectorOfVectorsFromKeyVectors(std::vector<int> hashKeysVec) {
+		std::vector<std::vector<CircleBase*>> vectorsOfCircleBaseVector;
+		for (size_t currentKey = 0; currentKey < hashKeysVec.size(); currentKey++)
+		{
+			vectorsOfCircleBaseVector.push_back(gridMap[hashKeysVec[currentKey]]);
+		}
+		return vectorsOfCircleBaseVector;
+	}
 
-    }
+	//This function returns us all the nerby cells to a circle, this way we can handle collisons only in grids near
+	std::vector<CircleBase*> GetNerbyCellsObjects(CircleBase* circle) {
+		std::vector<CircleBase*> nerbyCellsVector;
+		sf::Vector2f pos = circle->getPosition();
+		float multiplier = 2.5;// !still need to work on the multipier for best performance!
+		//Get us the place on the 2d dimension, like (2,2), (69,34)... like pos.x=1000 and gridSize=500 so gridX is the second cell 
+		int gridX = static_cast<int>(pos.x / (circle->getRadius() * multiplier));
+		int gridY = static_cast<int>(pos.y / (circle->getRadius() * multiplier));
+		//we go over all the cells near the circle cell. the one up and the one down, and the one right and the one left. working like two dimensional array
+		for (short int y = -1; y <= 1; y++) // we do this because it is the cells near him. like if the cell cord is (3,3). so in x axis his neighbores is (4,3) and (2,3)
+		{
+			for (short int x = -1; x <= 1; x++)// we do this because it is the cells near him. like if the cell cord is (3,3). so in y axis his neighbores is (3,2) and (3,4)
+			{
+				int hashKey = hashFunction(gridX + x, gridY + y);//we make the nerby cell to hashFunction to work with it
+				if (gridMap.find(hashKey) != gridMap.end()) {//if not found will point to the end of the grid map. and if found it will show an iterator.
+					nerbyCellsVector.insert(nerbyCellsVector.end(), gridMap[hashKey].begin(), gridMap[hashKey].end()); //Insert the ball at the end of the vector, because there might be few circles in the same are we take all of them, thats means gridMap[hashKey].begin() -> gridMap[hashKey].end() so it can take all the ones in the same cell
+				}
+			}
+		}
+		return nerbyCellsVector; //returns a vector of all the cells that have objects in
+	}
 
+	//Finds if a point is landing on a specific circle. for mouse detection
+	CircleBase* IsInSpecificGridRadius(sf::Vector2i pointPos, int hashKey) {
+		std::vector<CircleBase*> circleVec = gridMap[hashKey];
+		for (auto& circle : circleVec) {
+			sf::Vector2f CirclePos = circle->getPosition();
+			double distance = std::sqrt(std::pow(CirclePos.x - pointPos.x, 2) + std::pow(CirclePos.y - pointPos.y, 2));
 
-    CircleBase* IsInRadius(sf::Vector2i pointPos) {
-        return grid.IsInGridRadius(pointPos); // Return nullptr if no ball contains the point
-    }
+			if (distance <= circle->getRadius()) {
+				return circle; // Return a pointer to the ball if the point is within the radius
+			}
+		}
+		
+		return nullptr; // Return nullptr if no ball contains the point
+	}
 
-    void MoveAndDraw(sf::RenderWindow& window, float fps, float elastic)
-    {
-
-        grid.clear();
-        for (auto& ball : ballsList) {
-            grid.InsertCircle(ball);
-        }
-        if (fps <= 0) {
-            fps = 60;
-        }  // 60 FPS if the frame rate is undefined
-        float deltaTime = 1 / fps;
-
-        for (auto& ball : ballsList)
-        {
-
-            ball->updatePosition(deltaTime);  // Ensure deltaTime is reasonable
-            ball->draw(window);
-        }
-        MultiThreadChecks(window, elastic);
-    }
-
-    void MultiThreadChecks(sf::RenderWindow& window, float elastic) { //Get position negative problem
-        int hashMapSize = grid.GetHashMapSize();
-        int threadsNum = 4;//How much threads will be
-        if (hashMapSize/threadsNum<1)
-        {
-            threadsNum = hashMapSize;
-        }
-        std::vector<std::thread> threadsVec;
-        int loopCount = 0;
-        for (size_t i = 0; i < threadsNum; i++)
-        {
-            std::vector<CircleBase*> objVec;
-            size_t inner = static_cast<rsize_t>(hashMapSize / threadsNum);
-            for (size_t j = 0; j < inner; j++)
-            {
-                objVec.push_back(ballsList[loopCount]);
-                loopCount+=1;
-            }
-            threadsVec.push_back(std::thread(std::bind(&ObjectsList::HandleCollisionsInRange, this, std::ref(window), elastic, std::ref(objVec))));
-        }
-        std::vector<CircleBase*> objVec;
-        for (size_t i = 0; i < hashMapSize%threadsNum; i++)//all the remaining ones
-        {
-            objVec.push_back(ballsList[loopCount]);
-            loopCount += 1;
-        }
-        threadsVec.push_back(std::thread(std::bind(&ObjectsList::HandleCollisionsInRange, this, std::ref(window), elastic, std::ref(objVec))));
-        for (size_t i = 0; i < threadsVec.size(); i++)
-        {
-            threadsVec[i].join();
-        }
-    }
-
+	//Finds if a point is landing on a specific grid. for mouse detection
+	CircleBase* IsInGridRadius(sf::Vector2i pointPos) {
+		CircleBase* circlePointer;
+		for (auto& keyAndCircle : gridMap) {
+			int hashKey = keyAndCircle.first;
+			 circlePointer = IsInSpecificGridRadius(pointPos, hashKey);
+			if (circlePointer!=nullptr)
+			{
+				return circlePointer;
+			}
+		}
+		return nullptr; // Return nullptr if no ball contains the point
+	}
 };
-
