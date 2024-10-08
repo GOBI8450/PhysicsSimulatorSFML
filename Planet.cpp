b10c9a1bb15da4da0b0be5512d08511f5c5ceedb
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2024-10-08 13:20:47 +0300

    Update Planet.cpp

--- Planet.cpp
+++ Planet.cpp
@@ -2,16 +2,37 @@
 #include <SFML/Graphics.hpp>
 #include <iostream> 
 #include "CircleShape.h"
+#include "BaseShape.h"
+
 
 // Base_Shape class inheriting from sf::Transformable
 class Planet : public CircleShape
 {
-public:
-  class Planet()
-
 
 private:
-    float radius; //float to not be too big
+    float innerGravity; //float to not be too big
 
 
+public:
+   Planet(sf::Color color, float gravity, double mass, float radius, float innerGravity)
+        : Circle(color, gravity, mass,radius), innerGravity(innerGravity)
+    {
+        setRadius(radius);
+        setFillColor(color);
+        setOrigin(radius, radius);
+        setPosition(radius,radius);
+        oldPosition = sf::Vector2f(radius,radius);
+        acceleration = sf::Vector2f(0, gravity * 100); //(x axis, y axis);
+    }
+
+    void ActiveGravityToObj(BaseShape* obj){
+      float gravityForce=obj.mass * 6.6743 * pow(10,11);
+      applyForce(gravityForce);      
+    }
+
+    void UpdateGravity(BaseShape* obj){
+      
+    }
 }
+
+
