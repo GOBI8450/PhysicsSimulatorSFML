48f10d446fc554bef78eb3b174642daec3bbec58
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2024-10-08 12:52:43 +0300

    Update Planet.cpp

--- Planet.cpp
+++ Planet.cpp
@@ -1,1 +1,17 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream> 
+#include "CircleShape.h"
+
+// Base_Shape class inheriting from sf::Transformable
+class Planet : public CircleShape
+{
+public:
+  class Planet()
+
+
+private:
+    float radius; //float to not be too big
+
+
+}
