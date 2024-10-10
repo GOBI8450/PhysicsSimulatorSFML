b4c7d6e7853a00d3f7a5b70215c888a4fad4e187
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2024-10-10 12:44:40 +0300

    Update Planet.cpp

--- Planet.cpp
+++ Planet.cpp
@@ -1,38 +1,47 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream> 
-#include "CircleShape.h"
-#include "BaseShape.h"
+#include "CircleBase.h"
 
-
-// Base_Shape class inheriting from sf::Transformable
-class Planet : public CircleShape
+class Planet:public Circle
 {
-
 private:
-    float innerGravity; //float to not be too big
-
+	float innerGravity;
 
 public:
-   Planet(sf::Color color, float gravity, double mass, float radius, float innerGravity)
-        : Circle(color, gravity, mass,radius), innerGravity(innerGravity)
-    {
-        setRadius(radius);
-        setFillColor(color);
-        setOrigin(radius, radius);
-        setPosition(radius,radius);
-        oldPosition = sf::Vector2f(radius,radius);
-        acceleration = sf::Vector2f(0, gravity * 100); //(x axis, y axis);
-    }
+	Planet(float radius, sf::Color color, sf::Vector2f pos, float gravity, double mass, float innerGravity) : Circle(radius, color, pos, gravity, mass), innerGravity(innerGravity) {
+		setRadius(radius);
+		setFillColor(color);
+		setOrigin(radius, radius);
+		setPosition(radius, radius);
+		oldPosition = sf::Vector2f(radius, radius);
+		acceleration = sf::Vector2f(0, gravity * 100); //(x axis, y axis)
+	}
 
-    void ActiveGravityToObj(BaseShape* obj){
-      float gravityForce=obj.mass * 6.6743 * pow(10,11);
-      applyForce(gravityForce);      
-    }
+	// Copy constructor
+	Planet(const Planet& other)
+		: Circle(other), innerGravity(other.innerGravity) {
+		// Copy SFML shape properties
+		setRadius(other.getRadius());
+		setFillColor(other.getFillColor());
+		setOrigin(other.getOrigin());
+		setPosition(other.getPosition());
 
-    void UpdateGravity(BaseShape* obj){
-      
-    }
-}
+		// Copy Circle class properties
+		oldPosition = other.oldPosition;
+		acceleration = other.acceleration;
+		mass = other.mass;
+		gravity = other.gravity;
+	}
 
+
+
+	void Gravitate(BaseShape* object ) {
+		sf::Vector2f distanceVec = GetPosition() - object->GetPosition();
+		float distanceLength = DistanceTwoPoints(object->GetPosition());
+		sf::Vector2f normalVector = distanceVec / distanceLength;
+		object->applyForce(normalVector*innerGravity);
+	}
+
+};
 
